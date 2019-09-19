95fdf7a19be64cff1fc3a8a04d920af2b63b30f0
benjaminli7 <47728809+benjaminli7@users.noreply.github.com>
2019-09-19 16:20:10 +0200

    Ajoutez des fichiers projet.

---
+++ ModelObjet/Realisateur.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ModelObjet
+{
+    public class Realisateur
+    {
+        public string NomRealisateur { get; set; }
+        public string PhotoRealisateur { get; set; }
+    }
+}
